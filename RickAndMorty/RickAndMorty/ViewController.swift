56d6d4f9fba7d28d7661bb96f1d1e4386759b0a9
Rodrigo Miyashiro <rodrigo.miyashiro@yahoo.com.br>
2020-12-03 02:36:46 -0300

    Initial commit

---
+++ RickAndMorty/RickAndMorty/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RickAndMorty
+//
+//  Created by Rodrigo Miyashiro on 03/12/20.
+//  Copyright © 2020 Rodrigo Miyashiro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
